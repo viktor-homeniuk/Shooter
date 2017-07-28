fdb6ac1e4241890262094518cd1dfb15a48850be
Viktor Gomeniuk <viktor.gomeniuk@gmail.com>
2017-07-28 18:06:07 +0300

    Setup new UE4 project.

---
+++ Source/Shooter/ShooterGameMode.h
@@ -0,0 +1,19 @@
+// Copyright 1998-2017 Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "ShooterGameMode.generated.h"
+
+UCLASS(minimalapi)
+class AShooterGameMode : public AGameModeBase
+{
+	GENERATED_BODY()
+
+public:
+	AShooterGameMode();
+};
+
+
+
