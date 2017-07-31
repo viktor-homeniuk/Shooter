e8c6b23e13bb8972e629f218b32abb60725b3f9b
Viktor Gomeniuk <viktor.gomeniuk@gmail.com>
2017-07-31 18:08:53 +0300

    Setup basic game character.

--- Source/Shooter/ShooterGameMode.cpp
+++ Source/Shooter/ShooterGameMode.cpp
@@ -5,11 +5,9 @@
 #include "ShooterCharacter.h"
 #include "UObject/ConstructorHelpers.h"
 
-AShooterGameMode::AShooterGameMode()
-	: Super()
-{
+AShooterGameMode::AShooterGameMode(): Super() {
 	// set default pawn class to our Blueprinted character
-	static ConstructorHelpers::FClassFinder<APawn> PlayerPawnClassFinder(TEXT("/Game/FirstPersonCPP/Blueprints/FirstPersonCharacter"));
+	static ConstructorHelpers::FClassFinder<APawn> PlayerPawnClassFinder(TEXT("/Game/Character/bpShooterCharacter"));
 	DefaultPawnClass = PlayerPawnClassFinder.Class;
 
 	// use our custom HUD class
